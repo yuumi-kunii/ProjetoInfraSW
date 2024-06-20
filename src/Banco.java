5c825b4156ada47b0e749223e24674343c0f1e04
yuumi-kunii <yuumikunii1403@gmail.com>
2024-06-20 06:36:14 -0300

    Remoção Main e mais threads no problema 1

--- src/Banco.java
+++ src/Banco.java
@@ -19,7 +19,7 @@ public class Banco {
                     System.out.println("O saldo atual é: " + saldo);
                 }
                 finally {
-                    Thread.sleep(3);
+                    Thread.sleep(4000);
                     lock.unlock();
                 }
             }
@@ -99,7 +99,7 @@ public class Banco {
 
 
         public static void main(String[] args) throws InterruptedException {
-            Conta Conta1 = new Conta(5);
+            Conta Conta1 = new Conta(1500);
 
             Usuario TioPatinhas = new Usuario("Tio Patinhas");
             Usuario PatoDonald = new Usuario("Pato Donald");
@@ -114,25 +114,52 @@ public class Banco {
             Saque Saque2 = new Saque(Conta1, Zezinho, 689);
             Deposito Deposito2 = new Deposito(Conta1, PatoDonald, 956);
             Saque Saque3 = new Saque(Conta1, Luisinho, 236);
-
+            Saque Saque4 = new Saque(Conta1, Zezinho, 1678);
+            Deposito Deposito3 = new Deposito(Conta1, PatoDonald, 325);
+            Saque Saque5 = new Saque(Conta1, Luisinho, 795);
+            Saque Saque6 = new Saque(Conta1, Huguinho, 1234);
+            Saque Saque7 = new Saque(Conta1, Zezinho, 689);
+            Deposito Deposito4 = new Deposito(Conta1, PatoDonald, 164);
+            Saque Saque8 = new Saque(Conta1, Luisinho, 236);
 
             Thread TD1 = new Thread(Deposito1);
             Thread TD2 = new Thread(Saque1);
             Thread TD3 = new Thread(Saque2);
             Thread TD4 = new Thread(Deposito2);
             Thread TD5 = new Thread(Saque3);
+            Thread TD6 = new Thread(Saque4);
+            Thread TD7 = new Thread(Deposito3);
+            Thread TD8 = new Thread(Saque5);
+            Thread TD9 = new Thread(Saque6);
+            Thread TD10 = new Thread(Saque7);
+            Thread TD11 = new Thread(Deposito4);
+            Thread TD12 = new Thread(Saque8);
 
             TD1.start();
             TD2.start();
             TD3.start();
             TD4.start();
             TD5.start();
+            TD6.start();
+            TD7.start();
+            TD8.start();
+            TD9.start();
+            TD10.start();
+            TD11.start();
+            TD12.start();
 
             TD1.join();
             TD2.join();
             TD3.join();
             TD4.join();
             TD5.join();
+            TD6.join();
+            TD7.join();
+            TD8.join();
+            TD9.join();
+            TD10.join();
+            TD11.join();
+            TD12.join();
 
             System.out.println("O saldo final é:" + Conta1.saldo);
         }
